11259a019b7189691273444626305bdae8075993
Eliud Njuguna Kamau <spacetecictsolutions@gmail.com>
2016-10-06 01:03:13 +0300

    Initial commit

---
+++ app/src/main/java/rhynix/eliud/jsonrecyclerview/DataFish.java
@@ -0,0 +1,13 @@
+package rhynix.eliud.jsonrecyclerview;
+
+/**
+ * Created by eliud on 9/27/16.
+ */
+
+public class DataFish {
+    public String fishImage;
+    public String fishName;
+    public String catName;
+    public String sizeName;
+    public int price;
+}
